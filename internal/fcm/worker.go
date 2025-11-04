bf33d8dd3e1aa411a4df0181908d7c7b12146908
Ali Aizad <aliaizad72@gmail.com>
2025-11-04 10:18:13 +0800

    Add logging to worker

--- internal/fcm/worker.go
+++ internal/fcm/worker.go
@@ -14,19 +14,27 @@ import (
 func Worker(id int, jobsChan <-chan string, slotsChan chan<- struct{}) {
 	telemetry.Log(zerolog.InfoLevel, fmt.Sprintf("[Worker %v] spawned", id))
 	for jobId := range jobsChan {
+		log(zerolog.InfoLevel, id, jobId, "received jobId")
+
 		data, err := datastore.GetPayloadFromJobId(context.Background(), jobId)
 		if err != nil {
-			telemetry.Log(zerolog.ErrorLevel, fmt.Sprintf("ERROR RETRIEVING PAYLOAD FOR JOB %v: %v", jobId, err))
+			log(zerolog.ErrorLevel, id, jobId, fmt.Sprintf("ERROR RETRIEVING PAYLOAD: %v", err))
 			return
 		}
+		log(zerolog.DebugLevel, id, jobId, "retrieve payload")
 
 		var notification pb.NotificationPackage
 		err = proto.Unmarshal(data, &notification)
 		if err != nil {
-			telemetry.Log(zerolog.ErrorLevel, fmt.Sprintf("ERROR UNMARSHALLING NOTIFICATION %v: %v", jobId, err))
+			log(zerolog.ErrorLevel, id, jobId, fmt.Sprintf("ERROR UNMARSHALLING NOTIFICATION:: %v", err))
 			return
 		}
+		log(zerolog.DebugLevel, id, jobId, "unmarshalled payload data")
 
 		slotsChan <- struct{}{}
 	}
+}
+
+func log(level zerolog.Level, id int, jobId string, msg string) {
+	telemetry.Log(level, fmt.Sprintf("[Worker %v] [%v] %v", id, jobId, msg))
 }
